30bd88e143d06cc7566547e48bb76f3b5b194b4f
Be3pis <75560385+Be3pis@users.noreply.github.com>
2021-01-11 14:50:40 +0300

    Update Admin.lua

--- Admin.lua
+++ Admin.lua
@@ -1282,9 +1282,13 @@ Unknown.AddCommand("Claim", "Claimes a player", function(player)
    for i = 1,10 do
    wait()
    char:WaitForChild("HumanoidRootPart").CFrame = oldpos
-   local claim = Instance.new("IntValue")
-   claim.Name = "-Claimed"
-   claim.Parent = Player.Character
+
+                             if not Player.Character:FindFirstChild("-Claimed") then
+                                local Claimed = Instance.new("ObjectValue")
+                                Claimed.Name = "-Claimed"
+                                Claimed.Parent = Player.Character
+                            end
+                                      
    end
     end
     else
