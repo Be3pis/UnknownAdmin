7f1e58ca417d0207f4a9f66dec102bf0f86f50b7
Be3pis <75560385+Be3pis@users.noreply.github.com>
2021-01-12 09:15:33 +0300

    Update Admin.lua

--- Admin.lua
+++ Admin.lua
@@ -1280,8 +1280,6 @@ Unknown.AddCommand("Claim", "Claimes a player", function(player)
    char:WaitForChild("Humanoid"):Remove()
    Instance.new("Humanoid", char)
    char:WaitForChild("Head"):Destroy()
-   char:WaitForChild("Left Leg"):Destroy()
-   char:WaitForChild("Right Leg"):Destroy()
    spawn(function()
 
    while game:GetService("RunService").Heartbeat:wait(0.000000e+9) do
@@ -1315,6 +1313,8 @@ Unknown.AddCommand("Claim", "Claimes a player", function(player)
    char:WaitForChild("Torso"):Destroy()
    char:WaitForChild("Right Arm"):Destroy()
    char:WaitForChild("Left Arm"):Destroy()
+   char:WaitForChild("Left Leg"):Destroy()
+   char:WaitForChild("Right Leg"):Destroy()
    for i = 1,10 do
    wait()
    char:WaitForChild("HumanoidRootPart").CFrame = oldpos
