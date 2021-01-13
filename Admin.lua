22b609390e31f8664e0c9e9866407f00f3f48dc4
Be3pis <75560385+Be3pis@users.noreply.github.com>
2021-01-13 10:21:26 +0300

    Update Admin.lua

--- Admin.lua
+++ Admin.lua
@@ -1268,10 +1268,12 @@ Unknown.AddCommand("Claim", "Claimes a player", function(player)
 
                         Tool.Parent = LocalPlayer.Character
                         if Tool:FindFirstChild("Handle") and Player.Character.PrimaryPart ~= nil then
+                            oldposofcharacter = Player.Character.HumanoidRootPart.CFrame
                             repeat
-                                LocalPlayer.Character.HumanoidRootPart.CFrame = Player.Character.HumanoidRootPart.CFrame * CFrame.new(-2,0,1    )
+                                Player.Character.HumanoidRootPart.CFrame = Tool.Handle.CFrame
                                 game:GetService("RunService").Stepped:Wait()
                             until Tool.Parent == Player.Character
+                            LocalPlayer.Character.HumanoidRootPart.CFrame = oldposofcharacter
 
                             Workspace.FallenPartsDestroyHeight = 0/1/0
                             local CurrentCFrame = LocalPlayer.Character.HumanoidRootPart.CFrame
