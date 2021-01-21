c4021d76fe89186e18f85bdbceac37c3f0df516a
Be3pis <75560385+Be3pis@users.noreply.github.com>
2021-01-21 11:36:37 +0300

    Update Admin.lua

--- Admin.lua
+++ Admin.lua
@@ -1320,7 +1320,7 @@ Unknown.AddCommand("Claim", "Claimes a player", function(player)
 								local CurrentCFrame = LocalPlayer.Character.HumanoidRootPart.CFrame
 				repeat
 					LocalPlayer.Character.HumanoidRootPart.CFrame = CurrentCFrame - Vector3.new(0, 100000, 0)
-					Player.Character.HumanoidRootPart.CFrame = LocalPlayer.Character["Right Arm"].CFrame * CFrame.new(-1,-0.5,0)
+					Player.Character.HumanoidRootPart.CFrame = LocalPlayer.Character["Right Arm"].CFrame
 					wait()
 				until Tool.Parent == Player.Character
 
